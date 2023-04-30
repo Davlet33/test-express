97bcd7cda2849f704cbd888d93c7fedf9dd99070
Davlet Guseynov <ali_magomedov_2000@inbox.ru>
2023-05-01 01:06:46 +0300

    init commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express,{Request,Response} from 'express';
 import http from 'http';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 const server = http.createServer(app);
 
 app.get('/', (req:Request, res:Response):void => {
